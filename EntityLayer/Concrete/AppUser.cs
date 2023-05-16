0bac22968806881d7c5c0453a9b1a89798e81c63
onuryilmaaz <onuryilm.41@gmail.com>
2023-05-16 13:20:56 +0300

    Ders 11 sonu

--- EntityLayer/Concrete/AppUser.cs
+++ EntityLayer/Concrete/AppUser.cs
@@ -10,7 +10,7 @@ namespace EntityLayer.Concrete
     public class AppUser:IdentityUser<int>
     {
         public string Name { get; set; }
-        public string SurName { get; set; }
+        public string Surname { get; set; }
         public string District { get; set; }
         public string City { get; set; }
         public string ImageUrl { get; set; }
